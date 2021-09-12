ed46b414e5614d1c9c35896d01d47cd8963aa1e5
abbasam <tiggerwitht1378@gmail.com>
2021-09-12 09:08:34 +0430

    calculater commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function showOutput(val) {
+    document.getElementById("output").value = val;
+}
+
+function operation(val) {
+    document.getElementById("output").value += val;
+}
+
+function e() {
+    try {
+        showOutput(eval(document.getElementById("output").value))
+    } catch (e) {
+        showOutput('Error')
+    }
+}
